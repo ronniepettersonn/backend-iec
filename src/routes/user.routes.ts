5dd445279feaa35df13830980403ce2240a59d4f
ronniepettersonn <petterson28@hotmail.com>
2025-05-27 05:30:40 -0300

    alteraçao para usar banco em produção

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,14 +1,16 @@
 import { Router } from 'express'
 import { isAuthenticated } from '../middlewares/isAuthenticated'
-import { deleteUser, listUsers, updateUser, updateUserRole } from '../controllers/user.controller'
+import { deleteUser, listUsers, updateProfilePicture, updateUser, updateUserRole } from '../controllers/user.controller'
 import { hasRole } from '../middlewares/hasRole'
 import { Role } from '../@types/roles'
+import { upload } from '../middlewares/multer'
 
 const router = Router()
 
 router.use(isAuthenticated)
 
 router.get('/', listUsers)
+router.post('/profile/avatar', upload.single('file'), updateProfilePicture);
 router.put('/:id', hasRole( Role.ADMIN, Role.PASTOR),updateUser)
 router.delete('/:id', hasRole( Role.ADMIN, Role.PASTOR),deleteUser)
 router.patch('/:id/role', hasRole( Role.ADMIN, Role.PASTOR),updateUserRole)
